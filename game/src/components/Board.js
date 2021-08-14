51756b6bec075007d425c872eb18b046668b9150
1chowdhurai <raiyaan.rahman01@gmail.com>
2021-08-14 01:11:30 -0400

    Trying to add select piece functionality

--- game/src/components/Board.js
+++ game/src/components/Board.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Square from './Sqaure';
 
-
+let pieceSelected = "none";
 
 function nextChar(c) {
     return String.fromCharCode(c.charCodeAt(0) + 1);
 }
 
+const selectPiece = ([piece, player]) => {
+    if(piece == "") {
+        pieceSelected = "none";
+    }
+    if(player == "white" && piece.charAt(0) == 'w'){
+        pieceSelected = piece;
+    }
+    else if (player == "black" && piece.charAt(0) == 'b'){
+        pieceSelected = piece;
+    }
+
+    console.log("pieceSelected: " + piece);
+}
+
 const Board = (props) => {
     const board = [];
-    let row, squareType, letter, firstSquareLight = true;
+    let player = "white";
     
+    let row, squareType, letter, pieceName, firstSquareLight = true;
+    let [currentSquareTypes, setSquareTypes] = useState({});
+
+    let newSquareTypes = {};
     for(let i = 0; i < 8; i++){
-        row = [];
         letter = "a";
         let lightSquare = firstSquareLight;
         for(let j = 0; j < 8; j++){
@@ -22,13 +39,39 @@ const Board = (props) => {
             else {
                 squareType = "dark-square";
             }
+            newSquareTypes[letter+(8-i)] = squareType;
 
-            row.push(<Square squareType = {squareType} backgroundImage = {props.data[letter+(8-i).toString()]} key = {letter+(8-i)}/>);
+
             letter = nextChar(letter);
             lightSquare = !lightSquare;
         }
         firstSquareLight = !firstSquareLight;
+    }
+
+    console.log(newSquareTypes);
+    setSquareTypes(newSquareTypes);
+    
+    for(let i = 0; i < 8; i++){
+        row = [];
+        letter = "a";
+        
+        for(let j = 0; j < 8; j++){
+            
+
+            pieceName = props.data[letter+(8-i)];
+            // if(pieceSelected == pieceName) {
+            //     squareType += "-highlighted";
+            // }
+
+            // console.log(letter+(8-i).toString() + " : " + pieceName + ", pieceSelected:" + pieceSelected);
+
+            row.push(<Square squareType = {currentSquareTypes[letter+(8-i)]} backgroundImage = {pieceName} onClickFunction={selectPiece} onClickFunctionName="selectPiece" onClickParameters={[pieceName, player]} key = {letter+(8-i)}/>);
+            letter = nextChar(letter);
+        }
+        
         board.push(<div key={8-i} className="board-row">{row}</div>);
+
+        
     }
 
     return (
--- game/src/components/Sqaure.js
+++ game/src/components/Sqaure.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Square = (props) => {
     return (
-        <button className={"square " + props.squareType + " " + props.backgroundImage}></button>
+        <button className={"square " + props.squareType + " " + props.backgroundImage} onClick={() => {props.onClickFunction(props.onClickParameters)}}></button>
     );
 };
 
