725bb71672c6f6c5a6445bd077d53ec5e7438670
1chowdhurai <raiyaan.rahman01@gmail.com>
2021-09-05 15:38:52 -0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# AI-Plays-Chess
-A chess web app made with React and includes an AI I designed myself
--- game/src/components/Sqaure.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Square = (props) => {
-    return (
-        <button className={"square " + props.squareTypes[props.squareLoc] + " " + props.backgroundImage} onClick={() => {props.onClickFunction(props.onClickParameters)}}></button>
-    );
-};
-
-export default Square;
